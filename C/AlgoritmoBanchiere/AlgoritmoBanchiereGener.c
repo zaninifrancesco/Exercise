a4d2cdde5bf388440aeebe5cfd2812c471e5d689
Francesco Zanini <zaninifrancesco03@gmail.com>
2020-11-29 14:32:41 +0100

    Algoritmo banchiere generalizzato non funzionante

---
+++ C/AlgoritmoBanchiere/AlgoritmoBanchiereGener.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+
+
+
+// disp = rtot(a)(b) - assegnate(a)(b)
+void loadResTot(int resTot(3)){
+  for(int i = 0; i < 3; i++){
+    printf("%s\n","Inserire resTot");
+    scanf("%d", &resTot[i]);
+  }
+
+void assignRes(int ass[][])
+
+
+}
+
+int main() {
+  int nProc;
+
+  int max[nProc][3];
+  int needed[nProc][3];
+  int assigned[nProc][3];
+  int rTot[3];
+  int available[3];
+
+
+
+  printf("%s\n", "Inserire il numero dei processi: ");
+  scanf("%d", &nProc);
+
+  loadResTot(rTot);
+
+
+  return 0;
+}
