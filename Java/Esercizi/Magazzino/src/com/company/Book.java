22657f6f5bed5cb1925e673205dbe6e04d66ca83
Francesco Zanini <zaninifrancesco03@gmail.com>
2020-12-30 12:29:13 +0100

    Class Book.java done

--- Java/Esercizi/Magazzino/src/com/company/Book.java
+++ Java/Esercizi/Magazzino/src/com/company/Book.java
@@ -1,5 +1,29 @@
 package com.company;
 
 public class Book {
+    private int numPages;
+    private String title;
+    private String genre;
 
+    public Book(int nPages, String bookTitle, String bookGenre){
+        this.genre = bookGenre;
+        this.numPages = nPages;
+        this.title = bookTitle;
+    }
+
+    public int getNumPages() {
+        return numPages;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String showInfo(){
+        return "Book title: " + this.title + "\nBook pages: " + this.numPages + "\nBook genre: " + this.genre;
+    }
 }
