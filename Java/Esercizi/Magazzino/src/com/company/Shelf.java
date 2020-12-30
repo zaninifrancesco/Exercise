b3dc06a7f333a9871824a714fbfcf9962520d65d
Francesco Zanini <zaninifrancesco03@gmail.com>
2020-12-30 13:56:53 +0100

    Working on Shelf.java

--- Java/Esercizi/Magazzino/src/com/company/Shelf.java
+++ Java/Esercizi/Magazzino/src/com/company/Shelf.java
@@ -1,5 +1,22 @@
 package com.company;
 
 public class Shelf {
+    private int numBooks = 0;
+    private int id;
+    private boolean isFull = false;
+    private Book[] books = new Book[10];
 
+    public Shelf(int identifier){
+        this.id = identifier;
+    }
+
+    public void addBook(Book book){
+        if(numBooks < books.length){
+            books[numBooks] = book;
+            numBooks++;
+        }
+        else{
+            isFull = true;
+        }
+    }
 }
