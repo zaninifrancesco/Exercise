9f7109412f4c960f7d2bce2d477753219d944f7b
Francesco Zanini <zaninifrancesco03@gmail.com>
2020-12-31 11:06:36 +0100

    Sorting Algorithm Implemented

--- Java/Esercizi/Magazzino/src/com/company/Shelf.java
+++ Java/Esercizi/Magazzino/src/com/company/Shelf.java
@@ -36,4 +36,31 @@ public class Shelf {
     public Book[] getBooks() {
         return books;
     }
+
+    public void sortBooks(){
+        for(int i = 0; i < numBooks; i++){
+            for(int j = 0; j < numBooks; j++){
+                if(books[i].getNumPages() > books[j].getNumPages()){
+                    int pages1, pages2;
+                    String title1, genres1, title2, genres2;            //VARIABILI DI APPOGGIO PER FARE IL SELECTION SORT
+
+                    pages1 = books[i].getNumPages();
+                    pages2 = books[j].getNumPages();
+                    title1 = books[i].getTitle();
+                    title2 = books[j].getTitle();
+                    genres1 = books[i].getGenre();
+                    genres2 = books[j].getGenre();
+
+                    books[i].setNumPages(pages2);
+                    books[j].setNumPages(pages1);
+                    books[i].setTitle(title2);
+                    books[j].setTitle(title1);
+                    books[i].setGenre(genres2);
+                    books[j].setGenre(genres1);
+
+                }
+            }
+        }
+
+    }
 }
